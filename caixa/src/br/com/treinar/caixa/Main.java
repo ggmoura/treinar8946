77e4e44ed25ef16044cf6735f6b1fcccd63e5994
ADM <ADM@SL03PROFESSOR>
2017-01-12 22:27:11 -0200

    Inicio do projeto

---
+++ caixa/src/br/com/treinar/caixa/Main.java
@@ -0,0 +1,11 @@
+package br.com.treinar.caixa;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Olah Jadine...");
+	}
+	
+}
